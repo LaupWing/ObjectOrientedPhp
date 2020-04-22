f51030ada8fda763f5b833e0d7863b99b6fe8c81
LaupWing <loc_strict@hotmail.com>
2020-04-22 20:20:22 +0200

    done with object oriente

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
+    require('user_validator.php');
     if(isset($_POST['submit'])){
-        // 
-        echo 'form submitted';
+        $validation = new UserValidator($_POST);
+        $errors = $validation->validateForm();
     }
 ?>
 <!DOCTYPE html>
@@ -17,11 +18,15 @@
         <h2>Create new user</h2>
         <form action="<?php echo $_SERVER['PHP_SELF']?>" method="POST">
             <label>Username:</label>
-            <input type="text" name="username">
-
+            <input type="text" name="username" value="<?php htmlspecialchars($_POST['username']) ?? '' ?>">
+            <div class="error">
+                <?php echo $errors['username'] ?? ''?>
+            </div>
             <label>Email:</label>
-            <input type="text" name="email">
-
+            <input type="text" name="email" value="<?php htmlspecialchars($_POST['email']) ?? '' ?>">
+            <div class="error">
+                <?php echo $errors['email'] ?? ''?>
+            </div>
             <input type="submit" value="submit" name="submit">
         </form>
     </div>
